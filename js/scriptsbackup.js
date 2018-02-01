b8bf627de6216d313f71ce5cd9520302ba3d1c22
Andy Grossberg <andy.grossberg@gmail.com>
2018-02-01 09:36:53 -0800

    remove unused code and clean up a bit more.

--- js/scriptsbackup.js
+++ js/scriptsbackup.js
@@ -1,8 +1,6 @@
 // Business Logic
-
-var access = false; // Global var that should be tracked in the Game object instead.
-                    // It's used to pass access from a terminal that's successfully
-                    // accessed.
+var pressedKey = "";
+var access = false;
 
 function ComputerTerminal (status, name, buildingLocationNumber, roomNumber, locationX, locationY, type, questionArray, answerArray, success, failure) {
   this.status = status;
@@ -20,12 +18,14 @@ function ComputerTerminal (status, name, buildingLocationNumber, roomNumber, loc
 
 ComputerTerminal.prototype.verifyAnswer = function (answer) { // pass in the answer as array or string
   var compare = 0;
-  // console.log("this answer is " + this.answerArray);
-  // console.log("your answer is " + answer);
+  console.log("this answer is " + this.answerArray);
+  console.log("your answer is " + answer);
   for (var i=0; i < answer.length; i++) {
     if (answer[i] === this.answerArray[i]) { compare ++;}
   }
   if (compare === this.answerArray.length) { return true; } else { return false;}
+  // return compare;
+  // if(this.answerArray === answer) {return true; } else { return false; }
 };
 
 function Denizen(name, pic, greet, questions, answer1, answer2, answer3, items, level) {
@@ -40,6 +40,66 @@ function Denizen(name, pic, greet, questions, answer1, answer2, answer3, items, 
   this.level = level; // if level is 5 then that's the CEO. You catch him and leave.
 }
 
+// function testAnswer(myPrompt, x){
+//   // if  (myPrompt.verifyAnswer(x)) { alert("It's true!"); }
+//   if  (myPrompt.verifyAnswer(x)) { return true; } else { return false;}
+// }
+
+
+function getAnswer(myPrompt){
+  var combo = "";
+  var x = "";
+  // alert("My question type is: " + myPrompt);
+  if (myPrompt.type ==="colors") {
+    for (var i = 0; i < myPrompt.questionArray.length; i++){
+      combo += myPrompt.questionArray[i] + "=";
+    }
+    combo = combo.slice(0, -1);
+    x = prompt(combo); // this is prompting the user ********
+    $("#row1").html("<h3>" + combo + "</h3>");
+    alert("Drumroll: ");
+    if  (myPrompt.verifyAnswer(x)) { alert("It's true!"); } // ***********************
+
+
+  } else if (myPrompt.type ==="questions") {
+    for (var i = 0; i < myPrompt.questionArray.length; i++){
+      combo += myPrompt.questionArray[i] + "\n";
+    }
+    x = prompt(combo);
+  } else if (myPrompt.type ==="keypad") {
+    alert("KEYPAD!");
+    for (var i = 0; i < (myPrompt.questionArray.length); i+=3){
+      combo += myPrompt.questionArray[i] + " : ";
+      combo += myPrompt.questionArray[i + 1] + " : ";
+      combo += myPrompt.questionArray[i + 2] + "\n";
+    }
+    x = prompt(combo);
+  } else return "Destroyed!";
+  return x;
+}
+
+function checkAnswer(answer, myAnswer){
+  if (myAnswer.type === "colors") {
+    alert(myAnswer.type);   // works!! **********************************
+    alert("Answer = " + answer);
+    answer=answer.split("");
+    alert("Answer is NOW = " + answer);
+    alert("Expecting: ") +  myAnswer.answerArray;
+    alert("myAnswer is :") + myAnswer;
+    if (answer != myAnswer.answerArray){
+      alert("That is NOT correct!");
+    }
+    alert("COLORS!");
+  } else if (myAnswer.type ==="questions") {
+    alert("QUESTIONS!");
+  } else if (myAnswer.type ==="keypad") {
+    alert("KEYPAD!");
+  } else return "Destroyed!";
+  // return x;
+  return;
+}
+
+
 // User Interface Logic
 
 function drawKeypad(){
@@ -48,6 +108,7 @@ function drawKeypad(){
     myI = "<h2>" + " " + i + " " + "</h2>";
     $("#" + i).append(myI);
   }
+  // $("#row3").show();
 }
 
 function showQuery(someTerminal){
@@ -58,8 +119,11 @@ function showQuery(someTerminal){
   var myColor = -1;
   if (someTerminal.type ==="colors") {
     for (var i = 0; i < someTerminal.questionArray.length; i++){
+      // combo += someTerminal.questionArray[i] + "=";
       combo += someTerminal.questionArray[i];
     }
+    // combo = combo.slice(0, -1);
+
     for (var i = 1; i < someTerminal.questionArray.length + 1; i++){
       if(someTerminal.questionArray[i-1] === "R") {
         $("#show"+i).css("background-color", "red");
@@ -74,41 +138,68 @@ function showQuery(someTerminal){
     var myButton = '<div class="col-md-2">' +
       '<button class="btn btn-primary" name="myButton" type="button" id="myButton">Submit</button>' +
     '</div>'; // type="submit"
+
     $("#formLine").html(myButton);
+
     $('#colorboxes').bind('click', function(event) {
+    // alert(event.target.id);
       myColor++;
       if (myColor > colors.length) { myColor = 0; }
+      // alert(event.target.id);
       if (event.target.id.substring(0, 4) === "tell" && event.target.id.length > 4){
         $("#" + event.target.id).css( "background-color", colors[myColor] );
+        // answerColors[event.target.id.substring(4, 1)-1] = colors[myColor];
+        // alert(event.target.id.substr(event.target.id.length - 1));
         var x = event.target.id.substr(event.target.id.length - 1);
         // alert("x = " + x);
         answerColors[x-1] = colors[myColor];
         console.log("The answer colors array is now: " + answerColors);
       }
     });
+
     $('#myButton').bind('click', function(event) {
+      // alert(event.target.id);
+      // alert("answer colors: " + answerColors);
+      // alert("Some terminal's type is: " + someTerminal.type);
+      // alert("Some terminal's answer is: " + someTerminal.answerArray);
       var ygody = someTerminal.verifyAnswer(answerColors);
+      // alert("y god y is: " + ygody);
       if(someTerminal.verifyAnswer(answerColors)) {
         access = true;
         $('#colorboxes').unbind();
         $('#myButton').unbind();
         alert("You solved the code! " + access);
+        // return access;
         return;
       } else {
         alert("You have failed!");
       }
     });
+
+    // x = prompt(combo); // this is prompting the user ********
+    // x = "none";
     x = combo;
+    // $("#colorboxes").append("<h2>" + combo + "</h2>");
+    // $("#show").append("<h2>" + combo + "</h2>");
     $("#colorboxes").css( "display", "block" );
+
+
+    // alert("Drumroll: ");
+    // if  (someTerminal.verifyAnswer(x)) { alert("It's true!"); }  // This verifies the answer!
   } else if (someTerminal.type ==="questions") {
     alert("questions");
+    // $("#questions").show();
     for (var i = 0; i < someTerminal.questionArray.length; i++){
       combo += someTerminal.questionArray[i] + "\n";
       combo1 += someTerminal.questionArray[i] + "<br><br>";
     }
+    // x = prompt(combo);
     x = "none";
+    // $("#questions").show(); // do I need this?
+
     $("#ask").append("<h2>" + combo1 + "</h2>");
     $("#question").css( "display", "block" );
+
   } else if (someTerminal.type ==="keypad") {
     alert("KEYPAD!");
     for (var i = 0; i < (someTerminal.questionArray.length); i+=3){
@@ -119,6 +210,9 @@ function showQuery(someTerminal){
       combo += someTerminal.questionArray[i + 2] + "\n";
       combo1 += someTerminal.questionArray[i + 2];
     }
+    // x = prompt(combo);
+    // $("#"+i).append("<h2>" + i  + "</h2>");
+    // $("#keypad").append("<h2>" + combo1  + "</h2>");
     x = "none";
     drawKeypad();
     $("#keypad").css( "display", "block");
@@ -126,14 +220,62 @@ function showQuery(someTerminal){
   return x;
 }
 
+function challenge(someTerminal){
+  if (someTerminal.type ==="colors") {
+    var myFormQ =
+    '<form id="colors" class="form-group terminal row">' +
+      '<div class="col-md-10">' +
+        '<input id="colorAnswers" type="text" name="colorAnswers" value="" class="form-control" placeholder="#" required>' +
+      '</div>' +
+      '<div class="col-md-2">' +
+        '<button class="btn btn-primary" type="submit" name="button">Submit</button>' +
+      '</div>' +
+    '</form>';
+      $("#formLine").html(myFormQ);
+      // $("#questions").show();
+      $("#formLine").css( "display", "block" );
+      alert("Got here!");
+  } else if (someTerminal.type ==="questions") {
+    var myFormQ =
+    '<form id="questions" class="form-group terminal row">' +
+      '<div class="col-md-10">' +
+        '<input id="answers" type="text" name="answers" value="" class="form-control" placeholder="#" required>' +
+      '</div>' +
+      '<div class="col-md-2">' +
+        '<button class="btn btn-primary" type="submit" name="button">Submit</button>' +
+      '</div>' +
+    '</form>';
+      $("#answer").html(myFormQ);
+      // $("#questions").show();
+      $("#answer").css( "display", "block" );
+  } else if (someTerminal.type ==="keypad") {
+
+    var myFormK =
+    '<form id="keys" class="form-group terminal row">' +
+      '<div class="col-md-10 keyA">' +
+        '<input id="keyAnswers" type="text" name="keyAnswers" value="" class="form-control" placeholder="#" required>' +
+      '</div>' +
+      '<div class="col-md-2 keyA">' +
+        '<button class="btn btn-primary" type="submit" name="button">Submit</button>' +
+      '</div>' +
+    '</form>';
+      $("#entry").append(myFormK);
+  }
+}
+
+// function talkToFolks (personObject, theirPic, greet, askThem){
+// (myName, myPic, myGreet, myQuestions, myAnswer1, myAnswer2, myAnswer3, myItems, myLevel)
 function talkToFolks (personObject){
   var key=0;
+  // var exit = false;
   $(document).on('keydown', function(e){
       key = e.which;
       console.log("key is: " + key);
       checkKey(key);
   });
+
   function checkKey(myKey) {
+
   // var myKey ="0"
     var newString = "";
     var newString1 = "";
@@ -157,26 +299,34 @@ function talkToFolks (personObject){
       $("#li3").css("color", "rgb(0,0,0)"); // was rgb(239, 242, 247)
       $("#li4").css("color", "rgb(0,0,0)"); // was rgb(239, 242, 247)
       $("#theyReply").empty();
+
       $("#theyReply").html("<strong><p>" +  newString + "</p></strong>"); // what is myString now?
+      console.log("my String is: " + newString);
     }
     if (myKey === 50) { // pressed 2
       $("#li1").css("color", "rgb(0,0,0)"); // was rgb(239, 242, 247)
       $("#li2").css("color", "rgb(239, 142, 147)"); // was rgb(239, 242, 247)
       $("#li3").css("color", "rgb(0,0,0)"); // was rgb(239, 242, 247)
       $("#li4").css("color", "rgb(0,0,0)"); // was rgb(239, 242, 247)
+
       $("#people").off();
         $("#theyReply").html("<strong>" + personObject.answer2[0] + "</strong><br>");
+        // $("#theyAsk").html(personObject.answer2[0]);
         newString = "<ol type='a'>";
         for (var i = 1; i < 6; i++){
           var i1 = i +10; // ************************
           newString += "<li id='li" + i1 + "'>" + personObject.answer2[i] + "</li>";
         }
         newString += "</ol>";
+        // $("#li2").css("color", "rgb(239, 242, 247)");
+        // $("#theyReply").append("<p>" +  newString + "</p>");
         $("#theyReply").html("<strong>" + personObject.answer2[0] + "</strong><br>");
         $("#theyReply").append("<p>" +  newString + "</p>");
+
         $(document).on('keypress', function(e){
             key = e.which;
             // console.log("key is: " + key);
+
             var x = Math.floor(Math.random() * 5) + 1;
             if (key === 97) {
               console.log("It's an a!");
@@ -239,6 +389,7 @@ function talkToFolks (personObject){
               $("#li14").css("color", "rgb(0,0,0)");
               $("#li11").css("color", "rgb(0,0,0)");
             }
+            // newString = "";
         });
     }
     if (myKey === 51) {
@@ -248,26 +399,34 @@ function talkToFolks (personObject){
       $("#li1").css("color", "rgb(0,0,0)"); // was rgb(239, 242, 247)
       $("#li2").css("color", "rgb(0,0,0)"); // was rgb(239, 242, 247)
       $("#li4").css("color", "rgb(0,0,0)"); // was rgb(239, 242, 247)
+
       $("#theyReply").html("<strong><p>" +  newString2 + "</p></strong>");
     }
+
     if (myKey === 52) {
       exit = true;
       $("#people").off();
         $("#people").css("display", "none");
       return;
     }
+
   } // end of function
+
+
   $(".container").css( "display", "none" );
   $(".decorateMe").css( "display", "block" );
   $("#picture").html( "<img src=" + personObject.pic + " alt='Their Picture' height: '100'>");
   $("#narration").html( "<p>You walk up and speak to " + personObject.name + ".</p>");
   $("#theyAsk").html("<p>" +  personObject.greet + "</p>");
+  // $("#youAnswer").html("<ol><li>" +  askThem[0] + "</li><li>" +  askThem[1] + "</li><li>" +  askThem[2] + "</li></ol>");
   $("#youAnswer").html("<ol><li id='li1'>" +  personObject.questions[0] + "</li><li id='li2'>" +  personObject.questions[1] + "</li><li id='li3'>" +  personObject.questions[2] + "</li><li>I'm done with you.</li></ol>");
+
+// talkToFolks(personObject);
+// alert("Hit the end of the function, right?");
 }
 
 $(document).ready(function() {
   var dudes = [];
-  // terminals are hardcoded.
   myTerminal = new ComputerTerminal("Locked", "Terminal", 1, 1, 10, 10, "colors", ["B","G","B","Y","B"], ["red","green","blue","yellow","blue"], "OPEN", "ALARM-5");
   myQuestionTerminal = new ComputerTerminal("Locked", "Terminal", 1, 1, 10, 10, "questions", ["What is your favorite color?", "1. Red", "2. Blue", "3. Purple"], ["3"], "OPEN", "ALARM-5");
   myKeypadTerminal = new ComputerTerminal("Locked", "Terminal", 1, 1, 10, 10, "keypad", [1,2,3,4,5,6,7,8,9], ["3"], "OPEN", "ALARM-5");
@@ -302,14 +461,16 @@ $(document).ready(function() {
   // *
   // *********************************
 
-  // var myKindOfTerminal = myTerminal ;
-  // showQuery(myKindOfTerminal); // call the Terminal object and ask its "questions."
+var myKindOfTerminal = myTerminal ;
+showQuery(myKindOfTerminal); // call the Terminal object and ask its "questions."
+
+// challenge(myKindOfTerminal); This is deprecated
 
   // *********************************
   // Call the function to pop up
   // a person, currentl person 0.
   // *
-  // * talkToFolks(dudes[0]); // <-- this routine also handles its input validation
+  // * talkToFolks(dudes[0]); <-- this routine also handles its input validation
   // *
   // *********************************
 
